a7393367b28172455333c0fb1bd484251956726a
Mark Alldritt <alldritt@latenightsw.com>
2021-02-24 09:40:23 -0800

    Fix some SPM packaging issues.

--- Sources/LNSSignalStrength/LNSSignalStrength.swift
+++ Sources/LNSSignalStrength/LNSSignalStrength.swift
@@ -15,6 +15,7 @@ import SwiftUI
 struct Divided<S: Shape>: Shape {
     var amount: CGFloat // Should be in range 0...1
     var shape: S
+    
     func path(in rect: CGRect) -> Path {
         shape.path(in: rect.divided(atDistance: amount * rect.height, from: .maxYEdge).slice)
     }
@@ -26,10 +27,17 @@ extension Shape {
     }
 }
 
-struct LNSSignalStrength: View {
-    var bars: Int = 3
-    var totalBars: Int = 5
-    var body: some View {
+public struct LNSSignalStrength: View {
+    
+    private var bars: Int
+    private let totalBars: Int
+
+    public init(bars: Int = 3, totalBars: Int = 5) {
+        self.bars = bars
+        self.totalBars = totalBars
+    }
+    
+    public var body: some View {
         HStack(alignment: .center, spacing: 2) {
             ForEach(0..<totalBars) { bar in
                 RoundedRectangle(cornerRadius: 3)
