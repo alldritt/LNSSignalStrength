c7a3ef5962389e963677c95a7248053612be7b5b
Mark Alldritt <alldritt@latenightsw.com>
2025-03-17 17:39:26 -0700

    Standardize min OS versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LNSSignalStrength",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v7)
+        .macOS(.v11), .iOS(.v14), .tvOS(.v13), .watchOS(.v7)
     ],
     products: [
         .library(
